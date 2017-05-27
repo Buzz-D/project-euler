85e426789a8322b34cf8c37b60c15b45f66870a6
sebastianoelke <basti.oelke@gmail.com>
2017-05-27 23:54:45 +0200

    Problem 1 gelöst

---
+++ problem-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*
+#  Finde die Summe aller Zahlen von 1 bis 1000 die ein vielfaches von 3 oder 5 sind
+# Antwort lautet 233168
+
+sum = 0
+for number in range(1000):
+    if number % 3 == 0 or number % 5 == 0:
+        sum+=number
+print(sum)
