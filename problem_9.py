7df4e7a9f99a991e63e6963b2fb3946e0d4dbaa1
sebastianoelke <basti.oelke@gmail.com>
2021-05-11 17:51:06 +0200

    problem 9 adjusted

--- problem_9.py
+++ problem_9.py
@@ -14,11 +14,13 @@ u = 1
 while True:
     v = (sum_of_triplet / (2 * u)) - u
     if v.is_integer():
-        x = int(u ** 2 - v ** 2)
-        y = int(2 * u * v)
-        z = int(u ** 2 + v ** 2)
+        x = u ** 2 - v ** 2
+        y = 2 * u * v
+        z = u ** 2 + v ** 2
         if x > 0 and y > 0 and z > 0:
             break
     u += 1
-
-print(x, y, z)
+print(
+    "The product of the triplet is %d with x = %d, y = %d and z = %d"
+    % (x * y * z, x, y, z)
+)
