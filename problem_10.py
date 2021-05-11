8d1630bfca7dd2eaceb058c905a91c2ecbf7e5a9
sebastianoelke <basti.oelke@gmail.com>
2021-05-11 18:03:40 +0200

    problem 10 added

---
+++ problem_10.py
@@ -0,0 +1,11 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+from util.prime import is_prime
+
+max_range = 2000000
+sum_of_primes = 2  # Already added the 2 so that only every second number can be checked for being a prime
+for i in range(3, max_range, 2):
+    if is_prime(i):
+        sum_of_primes += i
+print("The sum of all primes below %d is %d" % (max_range, sum_of_primes))
